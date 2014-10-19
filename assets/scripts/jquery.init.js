1b3c8dc37ff79a53d84bec40714aa311b4cf5947
cs <bartus.csongor@gmail.com>
2014-10-19 11:05:34 +0300

    Ready to add steps

--- assets/scripts/jquery.init.js
+++ assets/scripts/jquery.init.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   
   // Play
-  $('#start-tutorial .icon').click(function() {
-    $('html').addClass('start');
+  $('#start-tutorial .icon, #navigation-2 .icon:first-of-type').click(function() {
+    $('html').addClass('play');
   });
   
 });
