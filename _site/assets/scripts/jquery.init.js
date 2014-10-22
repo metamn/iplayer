4659958a4e3b743901989c57da56af3163e43324
cs <bartus.csongor@gmail.com>
2014-10-22 11:17:58 +0300

    REady to posistion the share

--- _site/assets/scripts/jquery.init.js
+++ _site/assets/scripts/jquery.init.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
   
+  // Share 
+  $('#navigation-2 .icon:last-of-type').click(function() {
+    $("#share").toggleClass('active');
+  });
+  
+  
   // Play
   $('#start-tutorial .icon, #navigation-2 .icon:first-of-type').click(function() {
     $('html').addClass('play');
